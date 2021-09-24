665844ca39c37aaad0a18ce76e59520bc6b951a6
Steven-Avila198 <80013670+Steven-Avila198@users.noreply.github.com>
2021-09-23 20:04:37 -0500

    Avance en qt
    Se carga la imagen, se toma su ancho y alto y se saca la intensidad de Red, Green y Blue de toda la imagen en matrices separadas

--- Proyecto_Qt/Proyecto_Qt.pro
+++ Proyecto_Qt/Proyecto_Qt.pro
@@ -2,6 +2,9 @@ TEMPLATE = app
 CONFIG += console c++11
 CONFIG -= app_bundle
 
+#CONFIG -= qt
 
+QT += core
+QT += gui
 SOURCES += \
         main.cpp
--- Proyecto_Qt/main.cpp
+++ Proyecto_Qt/main.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
 #include <QImage>
+#include <QString>
+
 using namespace std;
+
+//bool QImage::load(const QString &fileName, const char *format=nullptr);
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    //Solicitud de nombre para cargar la imagen
+
+    string filename = "";
+    cout <<"Ingrese el nombre del archivo jpg"<<endl;
+    cin >> filename;
+    string ruta = "../Proyecto_Qt/Banderas/"+filename;
+    QImage img(ruta.c_str());  //imagen ya cargada
+
+    int ancho = img.width (); // ancho de la imagen
+    int alto = img.height(); // altura de la imagen
+
+    int MatrizRed[ancho][alto];
+    //capa de color rojo de la imagen
+    for (int indx = 0; indx < img.width(); ++indx){
+        for(int indy = 0; indy < img.height(); ++indy){
+            //cout << indx << ", " << indy << " = " <<img.pixelColor(indx,indy).red() <<endl;
+            MatrizRed[indx][indy]= img.pixelColor(indx,indy).red();
+        }
+
+    }
+
+    int MatrizGreen[ancho][alto];
+    //capa de color verde de la imagen
+    for (int indx = 0; indx < img.width(); ++indx){
+        for(int indy = 0; indy < img.height(); ++indy){
+            cout << indx << ", " << indy << " = " <<img.pixelColor(indx,indy).green() <<endl;
+            MatrizGreen[indx][indy]= img.pixelColor(indx,indy).green();
+        }
+    }
+
+    int MatrizBlue[ancho][alto];
+    //capa de color azul de la imagen
+    for (int indx = 0; indx < img.width(); ++indx){
+        for(int indy = 0; indy < img.height(); ++indy){
+            cout << indx << ", " << indy << " = " <<img.pixelColor(indx,indy).blue() <<endl;
+            MatrizBlue[indx][indy]= img.pixelColor(indx,indy).blue();
+        }
+    }
+
+
+
+
     return 0;
 }
