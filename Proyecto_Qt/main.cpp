b9f11e77e9af2090dfef6c4b73438c3f5296b4ba
Steven-Avila198 <80013670+Steven-Avila198@users.noreply.github.com>
2021-09-24 23:17:00 -0500

    Matrices con memoria dinamica
    Se crearon matrices bidimensionales con memoria dinámica y se comento el codigo

--- Proyecto_Qt/main.cpp
+++ Proyecto_Qt/main.cpp
@@ -16,39 +16,47 @@ int main()
     string ruta = "../Proyecto_Qt/Banderas/"+filename;
     QImage img(ruta.c_str());  //imagen ya cargada
 
-    int ancho = img.width (); // ancho de la imagen
-    int alto = img.height(); // altura de la imagen
+    unsigned int ancho = img.width (); // ancho de la imagen
+    unsigned int alto = img.height(); // altura de la imagen
 
-    int MatrizRed[ancho][alto];
-    //capa de color rojo de la imagen
-    for (int indx = 0; indx < img.width(); ++indx){
-        for(int indy = 0; indy < img.height(); ++indy){
-            //cout << indx << ", " << indy << " = " <<img.pixelColor(indx,indy).red() <<endl;
-            MatrizRed[indx][indy]= img.pixelColor(indx,indy).red();
+    //-------------Asignacion de memoria dinamica para arreglo de punteros----------------//
+    unsigned int **Red=new unsigned*[ancho];
+    unsigned int **Green=new unsigned*[ancho];
+    unsigned int **Blue=new unsigned*[ancho];
+    //-------------------Esto para crear matrices bidimencionales con memoria dinamica----//
+
+    for (unsigned int indx = 0; indx < ancho; ++indx){
+
+        //----Asignacion de memoria para cada una de las filas de la matriz----//
+        Red[indx]=new unsigned [alto];
+        Green[indx]=new unsigned [alto];
+        Blue[indx]=new unsigned [alto];
+        //----------------------------------------------------------------//
+
+        for(unsigned int indy = 0; indy < alto; ++indy){
+            cout << indx << ", " << indy << " = " <<img.pixelColor(indx,indy).red() <<endl;
+
+            //capa de color rojo de la imagen
+            Red[indx][indy]= img.pixelColor(indx,indy).red();
+            //capa de color verde de la imagen
+            Green[indx][indy]= img.pixelColor(indx,indy).green();
+            //capa de color azul de la imagen
+            Blue[indx][indy]= img.pixelColor(indx,indy).blue();
         }
 
     }
+    /* //Ciclo para imprimir matriz de pixeles por color para comparar con los pixeles de la imagen original//
+    for(unsigned int i=0;i<ancho;i++)
+    {
+        for(unsigned int j = 0 ; j <alto;j++){
+            cout<<Green[i][j]<<" ";
+            }
+        cout<<endl;
+    }*/
 
-    int MatrizGreen[ancho][alto];
-    //capa de color verde de la imagen
-    for (int indx = 0; indx < img.width(); ++indx){
-        for(int indy = 0; indy < img.height(); ++indy){
-            cout << indx << ", " << indy << " = " <<img.pixelColor(indx,indy).green() <<endl;
-            MatrizGreen[indx][indy]= img.pixelColor(indx,indy).green();
-        }
-    }
-
-    int MatrizBlue[ancho][alto];
-    //capa de color azul de la imagen
-    for (int indx = 0; indx < img.width(); ++indx){
-        for(int indy = 0; indy < img.height(); ++indy){
-            cout << indx << ", " << indy << " = " <<img.pixelColor(indx,indy).blue() <<endl;
-            MatrizBlue[indx][indy]= img.pixelColor(indx,indy).blue();
-        }
-    }
-
-
-
+    delete [] Red;
+    delete [] Green;
+    delete [] Blue;
 
     return 0;
 }
